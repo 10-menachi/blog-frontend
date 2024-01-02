4775515af8533814b0f9be6195b6f50e03896c7e
Christian Timbe <bobitlmr188221@spu.ac.ke>
2024-01-02 11:21:54 +0300

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
